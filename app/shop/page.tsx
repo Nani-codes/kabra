99566da896867a6d531cdf873701acdd7afc0e9f
poornatejaMV <oornatejareddymv@gmail.com>
2026-02-03 13:25:30 +0530

    build fix

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,5 +1,10 @@
 import { ShopContent } from "@/components/ShopContent";
+import { Suspense } from "react";
 
 export default function ShopPage() {
-  return <ShopContent />;
+  return (
+    <Suspense fallback={null}>
+      <ShopContent />
+    </Suspense>
+  );
 }
